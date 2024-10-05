f2752ec47071656b4d37e4deb4d6874dd270b3f6
huythanh0x <huythanh0x@gmail.com>
2024-10-05 21:04:55 +0700

    Add HomePageController to handle /home endpoint - display all coupons

--- src/main/java/com/huythanh0x/training_thanhvh_java_spring_jwt_jpa/security/SecurityConfig.java
+++ src/main/java/com/huythanh0x/training_thanhvh_java_spring_jwt_jpa/security/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
                 .requestMatchers("/api/v1/auth/**").permitAll()
                 .requestMatchers("/error").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/coupons/**").permitAll()
+                .requestMatchers("/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
